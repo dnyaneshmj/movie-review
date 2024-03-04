7254a813c77e0181093bb1ab28dd68f285f0f920
dnyaneshmj <dnyaneshmahajan12@gmail.com>
2024-03-04 00:56:10 +0000

    html structure

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const MovieList = () =>{
+
+    useEffect(()=>{
+        //API call
+    })
+
+    return(
+        <>
+            List of Movies
+        </>
+    )
+
+}
+export default MovieList
