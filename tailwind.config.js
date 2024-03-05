7b4baf0a1f0cb9e5bd1f566f110c50ced49abc7f
dnyaneshmj <dnyaneshmahajan12@gmail.com>
2024-03-05 11:16:57 +0000

    added Oxanium font to match design

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Oxanium': ['Oxanium', 'sans-serif']
+      },
+    },
+    fontFamily: {
+      'Oxanium': ['Oxanium', 'serif'] 
+    },
   },
   plugins: [],
 }
